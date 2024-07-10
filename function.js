cc37a6b29da7bed79ba71eb9f33b2646fba96611
reagan505 <reagangachanja6@gmail.com>
2024-07-10 11:01:58 +0300

    function examples

---
+++ function.js
@@ -0,0 +1,37 @@
+function greet(params) {
+  // block of code
+
+  console.log("Reagan");
+}
+
+greet();
+
+
+const greet2 = (fname, IName, gender, age) =>{
+
+    console.log(fname, IName, gender, age);
+}
+
+greet2("Reagan \n", "Gachanja", "male", 50);
+
+// var name "Reagan";
+
+
+greet("Reagan","Gachanja");
+
+function displaySum() {
+  let a = 28
+  let b = 8
+  let sum = a + b
+
+  console.log (a + b)
+}
+
+displaySum();
+
+function displaySum(a , b) {
+//  let sum = a + b
+console.log(a + b);
+}
+
+displaySum(8 , 28);
